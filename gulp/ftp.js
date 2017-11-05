7bfa60007118aa5523f7aa8b218919156116c6d2
Pasha School <pasha.school888@gmail.com>
2017-11-05 18:19:18 +0200

    add deplou task for gulp

--- gulp/ftp.js
+++ gulp/ftp.js
@@ -1,13 +1,25 @@
-const gulp = require('gulp');
-const gutil = require('gulp-util');
-const ftp = require('gulp-ftp');
+const gulp = require( 'gulp' );
+const gutil = require( 'gulp-util' );
+const ftp = require( 'vinyl-ftp' );
+
+const FTP = require("../ftp_data.json");
  
-gulp.task('ftp', function () {
-    return gulp.src('app/*')
-        .pipe(ftp({
-            host: 'skm162.hostsila.org',
-            user: 'admin@agnertech.com',
-            pass: 'Qazwsx56'
-        }))
-        .pipe(gutil.noop());
-});
+gulp.task( 'deploy', function () {
+ 
+    var conn = ftp.create( {
+        host: FTP.host,
+        user: FTP.user,
+        password: FTP.password
+    } );
+ 
+    var globs = [
+        'app/css/**',
+        'app/js/**',
+        'app/index.html'
+    ];
+
+    return gulp.src( globs, { base: './app/', buffer: false } )
+        .pipe( conn.newer( '/agregator.agnertech.com' ) )
+        .pipe( conn.dest( '/agregator.agnertech.com' ) );
+ 
+} );
